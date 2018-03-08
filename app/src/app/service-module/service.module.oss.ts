056b6f2368c06c8a481d1e721a52fb131b067034
vm <vidhi@moonraft.com>
2018-03-08 16:43:39 +0530

    no message

---
+++ app/src/app/service-module/service.module.oss.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CreateServiceComponent} from '../secondary-components/create-service/create-service.component';//*
+
+
+@NgModule({
+    imports: [
+        CreateServiceComponent
+    ],
+    declarations: [
+        CreateServiceComponent
+    ]
+  })
+  export class ServiceOSSModule {
+}
