dcef3ed9b842551a8e5912fed6323c05724ca256
Hemanth KS <suprita@moonraft.com>
2018-03-12 13:50:50 +0530

    Updated code

--- app/src/app/service-module/service.module.declarations.internal.ts
+++ app/src/app/service-module/service.module.declarations.internal.ts
@@ -1,2 +1,1 @@
-export * from '../secondary-components/create-service/create-service.component';//*
-export * from '../pages/service-detail/service-detail.component';//*
+export * from '../secondary-components/create-service/internal/create-service.component';//*
