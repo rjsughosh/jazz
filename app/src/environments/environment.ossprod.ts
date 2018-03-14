53091fd476cd77ce1bec89f1fbff17ac8918dc5a
vm <vidhi@moonraft.com>
2018-03-14 21:22:00 +0530

    ng build —env=ossprod ng build —env=prod ng build —env=dev ng build —env=oss

--- app/src/environments/environment.ossprod.ts
+++ app/src/environments/environment.ossprod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     configFile: 'config/config.oss.json',
     baseurl: "https://cloud-api.corporate.t-mobile.com/api",
     envName : "oss"
