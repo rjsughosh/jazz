55f49185337c96e0ffe118ce1e96956e7a20f51b
Sughosh R J <sughosh@moonraft.com>
2018-03-26 17:21:18 +0530

    no message

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   baseurl: "https://cloud-api.corporate.t-mobile.com/api",
   envName : "jazz",
   multi_env:false,
-  serviceTabs:['overview','access control','cost','metrics','logs'],
+  serviceTabs:['overview','access control','metrics','logs'],
   environmentTabs:['overview','deployments','code quality','assets','logs']
 
 }; 
