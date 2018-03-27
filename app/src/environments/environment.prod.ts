00de6a2b0aaee7a2f6416d1b9466afda248df4af
Sughosh R J <sughosh@moonraft.com>
2018-03-27 15:12:38 +0530

    no message

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,27 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+function loadJSON(filePath) {
+  const json = loadTextFileAjaxSync(filePath, "application/json");
+  return JSON.parse(json);
+}
+
+function loadTextFileAjaxSync(filePath, mimeType) {
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  if (mimeType != null) {
+    if (xmlhttp.overrideMimeType) {
+      xmlhttp.overrideMimeType(mimeType);
+    }
+  }
+  xmlhttp.send();
+  if (xmlhttp.status == 200) {
+    return xmlhttp.responseText;
+  }
+  else {
+    return null;
+  }
+}
+
+export const environment = loadJSON('https://api.myjson.com/bins/16nufb');
