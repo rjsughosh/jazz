1d46cc0cb5ed53414bb75c062309394eb875d4c5
Hemanth KS <suprita@moonraft.com>
2018-04-03 16:47:20 +0530

    Deleted Temp Environments Deleted ossprod and dev environment

--- app/src/temp env/environment.dev.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    configFile: 'config/config.json',
-    baseurl: "https://dev-cloud-api.corporate.t-mobile.com/api",
-    envName : "jazz",
-    multi_env:false,
-    serviceTabs:['overview','access control','cost','metrics','logs'],
-    environmentTabs:['overview','deployments','code quality','assets','logs']
-  };
