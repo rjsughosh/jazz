3b4859b29ee02aa61c75e33052ef030b2b375e4c
Sughosh R J <sughosh@moonraft.com>
2018-08-09 14:55:12 +0530

    no message

--- app/src/app/pages/environment-assets/env-assets-section.component.ts
+++ app/src/app/pages/environment-assets/env-assets-section.component.ts
@@ -230,7 +230,7 @@ export class EnvAssetsSectionComponent implements OnInit {
 				}
         // +this.offsetval
         // https://dev-cloud-api.corporate.t-mobile.com/api/g338sqltls-dev/
-        this.subscription = this.http.post('/jazz/assets/search',payload).subscribe(
+        this.subscription = this.http.post('/jazz/assets/search', payload).subscribe(
         // ?service='+this.service.name+'&domain='+this.service.domain+'&environement='+this.env+'&limit='+this.limitValue+'&offset=1').subscribe(
           // https://cloud-api.corporate.t-mobile.com/api/jazz/assets?service=assets&domain=jazz&environement=prod&limit=10&offset=0
 					// this.subscription = this.http.get("https://api.myjson.com/bins/1ccgh3").subscribe(
