070e07c40d0dc979fb20c03a7e9047045e1145b1
Sughosh Jahagirdar <sughoshraviraj.jahagirdar1@t-mobile.com>
2019-03-11 14:39:07 +0530

    no message

--- app/commit.js
+++ app/commit.js
@@ -1,12 +1,40 @@
 var shell = require('shelljs');
- 
-if (!shell.which('git')) {
-  shell.echo('Sorry, this script requires git');
-  console.log('echo')
-  shell.exit(1);
+var fs = require('fs');
+
+
+var abc = shell.exec("git rev-parse --abbrev-ref HEAD");
+console.log('current branch: ',abc.stdout)
+
+setUpstream();
+commitSteps("oss","upstream-public");
+commitSteps("internal","upstream-delta");
+
+function setUpstream(){
+    let url_delta = `https://bitbucket.corporate.t-mobile.com/scm/cap/jazz_webapp_delta.git`;
+    let git_url_oss = `https://github.com/tmobile/jazz.git`;    
+    
+    shell.exec(`git remote add upstream-public ${git_url_oss}`);
+    shell.exec(`git remote add upstream-detla ${url_delta}`);
+    shell.exec(`git remote -v`);	
 }
 
-if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
-    shell.echo('Error: Git commit failed');
-    shell.exit(1);
+function commitSteps(target,upstream,branch){
+    if(target === "internal"){
+        fs.rename('.gitignore-internal', '.gitignore', function(err) {
+            if ( err ) console.log('ERROR: ' + err);
+        });
+    }
+    else if( target === "oss"){
+        fs.rename('.gitignore-public', '.gitignore', function(err) {
+            if ( err ) console.log('ERROR: ' + err);
+        });
+    }
+    shell.exec(`git checkout ${branch}`);
+    shell.exec(`git add .gitignore`);
+    shell.exec(`git commit -m commit-gitignore`);
+    shell.exec(`git rm -r —cached .`);
+    shell.exec(`git add .`);
+    shell.exec(`git commit -m commit-all-${target}-changes`);
+    shell.exec(`git push ${upstream} ${branch}`);
 }
+
